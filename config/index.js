93aa0cb8498aa0c6005a01d3cdd374273d147a81
lixiaobin <lixiaobin@baijiahulian.com>
2017-06-18 11:56:26 +0800

    ouput public path set

--- config/index.js
+++ config/index.js
@@ -4,10 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/main.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-chat/',///testCwz/vue/vue-chat/
+    assetsPublicPath: '',///testCwz/vue/vue-chat/
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
