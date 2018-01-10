9e7ca3ee43c19f35353adbacee42cb3232577ac4
lixiaobin <lixiaobin@baijiahulian.com>
2018-01-10 18:51:43 +0800

    活动列表

---
+++ src/modules/discounts/components/Coupons.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <div class="filter-wrap">
+            <div class="filter-box">
+                <el-input placeholder="请输入内容" style="width: 300px;" v-model.trim="key" class="input-with-select">
+                    <el-button slot="append" icon="el-icon-search"></el-button>
+                </el-input>
+            </div>
+        </div>
+        <el-table
+            ref="table"
+            v-if="list && list.length"
+            :data="list"
+            :highlight-current-row="true">
+            <el-table-column
+                label="卡劵名称"
+            >  
+                <template slot-scope="scope">
+                    <a href="javascript:;" @click="showNiticeItem(scope.row)">李小斌</a>
+                </template>
+            </el-table-column>
+            <el-table-column
+                prop="date"
+                label="卡券有效期">
+            </el-table-column>
+            <el-table-column
+                prop="date"
+                label="卡券规则">
+            </el-table-column>
+            <el-table-column
+                prop="date"
+                label="添加时间">
+            </el-table-column>
+            <el-table-column
+                prop="date"
+                label="已发卷">
+            </el-table-column>
+            <el-table-column
+                prop="date"
+                label="已使用">
+            </el-table-column>
+                <el-table-column
+                fixed="right"
+                label="操作">
+                <template slot-scope="scope">
+                    <el-tooltip placement="top" content="删除">
+                        <span
+                            @click="del"
+                            class="el-icon-edit-outline"></span>
+                    </el-tooltip>
+                    <el-tooltip placement="top" content="更新状态">
+                        <span 
+                            @click="updateStatus"
+                            class="el-icon-circle-plus"></span>
+                    </el-tooltip>
+                </template>
+            </el-table-column>
+        </el-table>
+        <div v-if="!list.length" class="none-list">
+            <span>暂无设置信息</span>
+        </div>
+        <pager 
+            @currentchange="changePage"
+            @sizechange="changeSize"
+            v-model="pageDto">
+        </pager>
+    </div>
+</template>
+
+<script>
+
+    import listPageDto from '../../../common/mixin/listPageDto';
+
+    export default {
+        mixins: [listPageDto],
+        data () {
+            return {
+                key: '',
+                activeName: '1',
+                view: 'Active',
+                list: [{}],
+                loading: false
+            }
+        },
+        methods: {
+            /**
+             * 删除活动 
+             */
+            del () {
+                this.$confirm('确认删除?', '提示', {
+                        type: 'warning'
+                    }).then(() => {
+                    });
+            },
+            /**
+             * 更新状态 
+             */
+            updateStatus () {
+                this.$confirm('确认启用?', '提示', {
+                        type: 'warning'
+                    }).then(() => {
+                    });
+            }
+        }
+    }
+</script>
--- src/modules/discounts/index.vue
+++ src/modules/discounts/index.vue
@@ -1,26 +1,39 @@
 <template>
-    <div class="material-list account-wrap" v-loading.fullscreen.lock="loading">
+    <div class="material-list account-wrap">
         <el-row class="module-title">
             <el-col :span="12">
                 <breadcrumb-nav :data="breadcrumb"></breadcrumb-nav>
             </el-col>
         </el-row>
+         <div class="list-box">
+            <el-tabs v-model="activeName">
+                <el-tab-pane label="活动" name="Active"></el-tab-pane>
+                <el-tab-pane label="卡劵" name="Coupons"></el-tab-pane>
+            </el-tabs>
+            <component :is="activeName"></component>         
+        </div>
     </div>
 </template>
 
 <script>
 
     import BreadcrumbNav from '../../common/components/BreadcrumbNav.vue';
+    import Active from './components/Active';
+    import Coupons from './components/Coupons';
 
     export default {
         data () {
             return {
+                activeName: 'Active',
                 breadcrumb: ['活动管理'],
-                loading: false
             }
         },
+        methods: {
+        },
         components: {
-            BreadcrumbNav
+            BreadcrumbNav,
+            Active,
+            Coupons
         }
     }
 </script>
