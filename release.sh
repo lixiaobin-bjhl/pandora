6712c24af479dca5e86c52d8da9abb08cf3c202b
zhangbing <zhangbing@baijiahulian.com>
2018-01-31 17:27:13 +0800

    自动构建测试

--- release.sh
+++ release.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+npm run build
+git add .
+git commit -m $1
+git push
 ssh sheliguo
+cd /root/lixiaobin/pandora/
+git checkout pc
+git pull origin pc
 
-echo 'build'
+cd /app/pandora/webroot/
+rm -rf static
+
+cp /root/lixiaobin/pandora/dist/* ./
+
+echo 'build end'
